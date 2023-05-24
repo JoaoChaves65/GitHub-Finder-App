b352a6290e7a06bf676c0271903b5e00488666c3
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-24 10:45:14 -0300

    Criando component Search

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.tsx';
-import Home from './routes/Home.tsx';
+import Home from './routes/Home.jsx';
 
 import './index.css';
 
